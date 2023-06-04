608c82225aa8736699d4d196386faf2517514174
DRTAG <masan.korea@gmail.com>
2023-06-04 15:01:47 +0900

    Update get_default_params.R

--- R/get_default_params.R
+++ R/get_default_params.R
@@ -2,7 +2,7 @@ get_default_params <- function() {
   model <- Sys.getenv("OPENAI_MODEL", "gpt-3.5-turbo")
   params <- list(
     model = model,
-    max_tokens = as.numeric(Sys.getenv("OPENAI_MAX_TOKENS", 256)),
+    #max_tokens = as.numeric(Sys.getenv("OPENAI_MAX_TOKENS", 256)),
     temperature = as.numeric(Sys.getenv("OPENAI_TEMPERATURE", 0.7)),
     top_p = as.numeric(Sys.getenv("OPENAI_TOP_P", 1)),
     frequency_penalty = as.numeric(Sys.getenv("OPENAI_FREQUENCY_PENALTY", 0.5)),
