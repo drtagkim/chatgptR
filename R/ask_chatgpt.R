062678c0fccc471482cf3e230d5a6c3ae7674903
DRTAG <masan.korea@gmail.com>
2023-06-04 20:45:03 +0900

    bug fix

--- R/ask_chatgpt.R
+++ R/ask_chatgpt.R
@@ -32,7 +32,7 @@ ask_chatgpt <- function(question,knowledge_dir=NULL,context=NULL,history_file=NU
   }
   result=parse_response(gpt_get_completions(
     question,
-    context=context,
+    context=mssg_context,
     conversation=append_lists(mssg_knowledge,mssg_previous)))
   if(update & !is.null(history_file)) {
     mssg_added1=list(list(role="user",content=question))
