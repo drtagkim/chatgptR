b7a976b16a95085c8668f1f4a74bc2a4a4d367a8
DRTAG <masan.korea@gmail.com>
2023-08-02 15:54:42 +0900

    Update

--- R/literal.R
+++ R/literal.R
@@ -1,7 +1,12 @@
+#' Literal data
+#'
+#' @export
+#'
 literal <- function(prompt=NULL) {
   list(
     no_api = "`OPENAI_API_KEY` not provided.",
-    gpt3_5 = "gpt-3.5-turbo-0613",
+    gpt3_5 = "gpt-3.5-turbo",
+    gpt3_5_16k = 'gpt-3.5-turbo-16k',
     gpt4 = "gpt-4",
     gpt4_32 = "gpt-4-32k",
     echo_user_input_info = paste0("\n ---- Your input ---- :\n\n", prompt, "\n"),
